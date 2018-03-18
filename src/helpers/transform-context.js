744e5cfcedc7d6f8e9f307d71db74faeb912929e
FireNeslo <fireneslo@gmail.com>
2018-03-18 21:17:15 +0100

    fixed scoping isssues

--- src/helpers/transform-context.js
+++ src/helpers/transform-context.js
@@ -10,55 +10,78 @@ delete GLOBALS.open
 
 export default function transformContext(code, map) {
   map = JSON.parse(map.toString())
+  const referenced = new Map()
+  const imported = []
+
   return transform(code, {
     inputSourceMap: map,
+    parserOpts: {
+      allowImportExportEverywhere: true
+    },
     plugins: [function transform() {
       return {
-         visitor: {
-           AssignmentExpression(path) {
-             var node = path.node.left
-             var parents = []
-             while(node.object) {
-               parents.push(node)
-               node = node.object
-             }
-             if(node.name in GLOBALS) return
-             if(path.scope.hasBinding(node.name)) return
-             if(node.name === 'this') return
+        visitor: {
+          ImportDeclaration(path) {
+            imported.push(path.node)
+            path.remove()
+          },
+          AssignmentExpression(path) {
+            var node = path.node.left
+            var parents = []
+            while(node.object) {
+              parents.push(node)
+              node = node.object
+            }
+            if(node.name in GLOBALS) return
+            if(path.scope.hasBinding(node.name)) return
+            if(node.name === 'this') return
 
-             var parent = parents.pop()
+            var parent = parents.pop()
 
-             if(parent) {
-               parent.object = memberExpression(identifier('this'), node)
-             } else {
-               path.node.left = memberExpression(identifier('this'), node)
-             }
-             path.replaceWith(path.node)
-           },
-           CallExpression(path) {
-             var {node, scope} = path
-             if(node.callee.type !== 'Identifier') return
-             if(node.callee.name in GLOBALS) return
-             if(path.scope.hasBinding(node.callee.name)) return
-             if(node.callee.name === 'this') return
-             if(node.callee.name === '$$') return
+            if(parent) {
+              parent.object = memberExpression(identifier('this'), node)
+            } else {
+              path.node.left = memberExpression(identifier('this'), node)
+            }
+            path.replaceWith(path.node)
+          },
+          CallExpression(path) {
+            var {node, scope} = path
+            if(node.callee.type !== 'Identifier') return
+            if(node.callee.name in GLOBALS) return
+            if(path.scope.hasBinding(node.callee.name)) return
+            if(node.callee.name === 'this') return
+            if(node.callee.name === '$$') return
 
-             node.callee = memberExpression(identifier('this'), node.callee)
-           },
-           ReferencedIdentifier(path) {
-             var {node, scope} = path
-             if(node.name in GLOBALS) return
-             if(path.scope.hasBinding(node.name)) return
-             if(node.name === 'this') return
-             if(node.name === '$$') return
+            node.callee = memberExpression(identifier('this'), node.callee)
+          },
+          ReferencedIdentifier(path) {
+            var {node, scope} = path
+            if(node.name in GLOBALS) return
+            if(scope.hasBinding(node.name)) return
+            if(node.name === 'this') return
+            if(node.name === '$$') return
 
-             scope.push(variableDeclarator(
-               identifier(node.name),
-               memberExpression(identifier('this'), identifier(node.name))
-             ));
-           }
-         }
-       }
+            referenced.set(scope, node.name)
+          },
+          Program: {
+            exit(path) {
+              for(const node of imported) {
+                path.node.body.unshift(node)
+              }
+
+              for(const [scope, name] of referenced) {
+                if(scope.hasBinding(name)) continue
+
+                scope.push(variableDeclarator(
+                  identifier(name),
+                  memberExpression(identifier('this'), identifier(name))
+                ))
+              }
+            }
+          }
+        }
+      }
     }]
   })
 }
